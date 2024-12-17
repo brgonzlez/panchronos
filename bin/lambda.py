f788374bd834355b466f36298235cb87752b9ea4
Bruno Romero <126069027+mudymudy@users.noreply.github.com>
2024-12-17 12:24:12 +0000

    Update lambda.py

--- bin/lambda.py
+++ bin/lambda.py
@@ -11,8 +11,14 @@ import pandas as pd
 import os
 
 
+# I need to make sure that every value in sys.argv[2] , sys.argv[3] and sys.argv[4] is a float variable.
 
 data = sys.argv[1]
+geneCompleteness = float(sys.argv[2])
+lowerBound = float(sys.argv[3])
+upperBound = float(sys.argv[4])
+
+
 base_filename = os.path.basename(data)
 output_file = f"{base_filename}_final.csv"
 
@@ -23,7 +29,7 @@ with open(data, 'r') as file:
 updated_series = []
 
 for index, row in summary.iterrows():
-    if row['completeness'] >= 50 and row['normalizedCoverage'] >= 0.5:
+    if row['completeness'] >= geneCompleteness and row['normalizedCoverage'] >= lowerBound and row['normalizedCoverage'] <= upperBound:
         updated_series.append(1)
     else:
         updated_series.append(0)
