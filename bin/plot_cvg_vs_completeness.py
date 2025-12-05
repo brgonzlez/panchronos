99154c8998911fff6085bb58f73ff06d656c7834
Bruno Romero <126069027+brgonzlez@users.noreply.github.com>
2025-12-05 16:22:32 +0000

    Update plot_cvg_vs_completeness.py

--- bin/plot_cvg_vs_completeness.py
+++ bin/plot_cvg_vs_completeness.py
@@ -5,10 +5,6 @@ Created on Mon Sep  9 16:14:48 2024
 
 @author: bruno
 
-
-TODO: 
-    I NEED TO FIX BOX POSITION AND MOVE IT TO THE RIGHT, line 63
-    I NEED TO INCORPORATE COMPLETENESS VALUES INTO THE GREY BOXES AND NOT HAVING 50 THERE
 """
 
 import pandas as pd
@@ -23,7 +19,7 @@ data = sys.argv[1]
 completeness_str = sys.argv[2]
 coverage_lower_str = sys.argv[3]
 coverage_upper_str = sys.argv[4]
-
+normalised_boundary = sys.argv[5]
 
 print(f"Data file: {data}")
 print(f"Completeness: {completeness_str}")
@@ -47,28 +43,49 @@ with open(data, 'r') as file:
 max_value = summary['normalizedGeneSimple'].max()
 
 line_segments = [
-    [(completeness, 0), (completeness, max_value)],  # Line 1: vertical line 
+    [(completeness, 0), (completeness, coverage_upper)],  # Line 1: vertical line 
     [(0, coverage_lower), (100, coverage_lower)], # Line 2: horizontal line for lower boundary
-    [(0, coverage_upper), (100, coverage_upper)]
+    [(0, coverage_upper), (100, coverage_upper)] # Line 3: horizontal line for upper boundary
 ]
 
+high_genes_count = (summary['normalizedGeneSimple'] > normalised_boundary).sum()
+plot_summary = summary[summary['normalizedGeneSimple'] <= normalised_boundary]
+
+
+plt.rcParams['mathtext.fontset'] = 'cm'  # Computer Modern
 plt.figure(figsize=[15,10])
-ax = sns.scatterplot(data=summary, y='normalizedGeneSimple' , x='geneCompleteness' , hue='sampleID', s=12)
+ax = sns.scatterplot(data=plot_summary, y='normalizedGeneSimple' , x='geneCompleteness' , hue='sampleID', s=20)
 
 lines = LineCollection(line_segments, linewidths=2, colors='black', linestyle='dashed')
 ax.add_collection(lines)
 
+# Adjust y-axis based on max_value and coverage thresholds
+if max_value > coverage_upper:
+    new_ymax = max_value
+else:
+    new_ymax = coverage_upper + 1
+
 
 # Create the Rectangle patch
-rec1 = Rectangle((0, 0), completeness, max_value, linewidth=1, edgecolor='none', facecolor='grey', alpha=0.3)
-rec2 = Rectangle((completeness, 0), completeness, coverage_lower, linewidth=1, edgecolor='none', facecolor='grey', alpha=0.3)
-rec3 = Rectangle((completeness, 0), completeness, coverage_upper, linewidth=1, edgecolor='none', facecolor='grey', alpha=0.3)
+rec1 = Rectangle((0, 0), completeness, coverage_upper, linewidth=1, edgecolor='none', facecolor='grey', alpha=0.3)  # completeness box
+rec2 = Rectangle((completeness, 0), 100 - completeness, coverage_lower, linewidth=1, edgecolor='none', facecolor='grey', alpha=0.3) # lower box
+rec3 = Rectangle((0, coverage_upper), 100, new_ymax, linewidth=1, edgecolor='none', facecolor='grey', alpha=0.3) # upper box
 
 # Add the rectangle to the plot 
 ax.add_patch(rec1)
 ax.add_patch(rec2)
 ax.add_patch(rec3)
-plt.legend(markerscale=1, fontsize=10, loc='center right', bbox_to_anchor=(1.7,0.969))
+
+plt.legend(markerscale=1, fontsize=10, loc='center right', bbox_to_anchor=(1.2,0.969))
+
+ax.set_ylim(0, new_ymax)
+
+# Force the plot to start at 0
+ax.set_xlim(left=0)
+
+ax.set_xlabel("Gene Completeness", fontsize=14)
+ax.set_ylabel("Normalized Gene Coverage", fontsize=14)
+ax.set_title("Normalised Coverage per Gene vs Gene Completeness", fontsize=16, pad=20)
 
 sample_id = summary['sampleID'].iloc[0]
 plt.savefig(f'plotCoverage_vs_Completeness_{sample_id}.png', bbox_inches='tight')
