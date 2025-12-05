4219883bdbd7c6dbe1b52fe4f92bb0511ca331a5
Bruno Romero <126069027+brgonzlez@users.noreply.github.com>
2025-12-05 13:16:25 +0000

    Update plot_cvg_vs_completeness.py

--- bin/plot_cvg_vs_completeness.py
+++ bin/plot_cvg_vs_completeness.py
@@ -21,16 +21,19 @@ from matplotlib.patches import Rectangle
 
 data = sys.argv[1]
 completeness_str = sys.argv[2]
-coverage_str = sys.argv[3]
+coverage_lower_str = sys.argv[3]
+coverage_upper_str = sys.argv[4]
 
 
 print(f"Data file: {data}")
 print(f"Completeness: {completeness_str}")
-print(f"Coverage: {coverage_str}")
+print(f"Coverage: {coverage_lower_str}")
+print(f"Coverage: {coverage_upper_str}")
 
 try:
     completeness = int(completeness_str)
-    coverage = float(coverage_str)
+    coverage_lower = float(coverage_lower_str)
+    coverage_upper = float(coverage_upper_str)
 except ValueError as e:
     print(f"Error converting arguments to float: {e}")
     sys.exit(1)
@@ -44,25 +47,28 @@ with open(data, 'r') as file:
 max_value = summary['normalizedGeneSimple'].max()
 
 line_segments = [
-    [(completeness, 0), (completeness, max_value)],  # Line 1: vertical line at x=50
-    [(0, coverage), (100, coverage)]  # Line 2: horizontal line at y=1.5
+    [(completeness, 0), (completeness, max_value)],  # Line 1: vertical line 
+    [(0, coverage_lower), (100, coverage_lower)], # Line 2: horizontal line for lower boundary
+    [(0, coverage_upper), (100, coverage_upper)]
 ]
 
 plt.figure(figsize=[15,10])
-ax = sns.scatterplot(data=summary, y='normalizedGeneSimple' , x='geneCompleteness' , hue='sampleID', s=7)
+ax = sns.scatterplot(data=summary, y='normalizedGeneSimple' , x='geneCompleteness' , hue='sampleID', s=12)
 
 lines = LineCollection(line_segments, linewidths=2, colors='black', linestyle='dashed')
 ax.add_collection(lines)
 
 
 # Create the Rectangle patch
-rec1 = Rectangle((0, 0), 50, max_value, linewidth=1, edgecolor='none', facecolor='grey', alpha=0.3)
-rec2 = Rectangle((50, 0), 50, 0.5, linewidth=1, edgecolor='none', facecolor='grey', alpha=0.3)
+rec1 = Rectangle((0, 0), completeness, max_value, linewidth=1, edgecolor='none', facecolor='grey', alpha=0.3)
+rec2 = Rectangle((completeness, 0), completeness, coverage_lower, linewidth=1, edgecolor='none', facecolor='grey', alpha=0.3)
+rec3 = Rectangle((completeness, 0), completeness, coverage_upper, linewidth=1, edgecolor='none', facecolor='grey', alpha=0.3)
 
 # Add the rectangle to the plot 
 ax.add_patch(rec1)
 ax.add_patch(rec2)
-plt.legend(markerscale=1, fontsize=10, loc='center right', bbox_to_anchor=(1.25,0.969))
+ax.add_patch(rec3)
+plt.legend(markerscale=1, fontsize=10, loc='center right', bbox_to_anchor=(1.7,0.969))
 
-
-plt.savefig('plotCoverage_vs_Completeness.png', bbox_inches='tight')
+sample_id = summary['sampleID'].iloc[0]
+plt.savefig(f'plotCoverage_vs_Completeness_{sample_id}.png', bbox_inches='tight')
